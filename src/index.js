7f33cda6563c6fa0027d4caedf414039b74c2ce1
leeshaun <kinhkha95@gmail.com>
2022-10-30 18:07:41 +0700

    totolist useReducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './UseReducer/App';
+import App from './UseReducer/Todolist';
 import reportWebVitals from './reportWebVitals';
 
 //fake comments
